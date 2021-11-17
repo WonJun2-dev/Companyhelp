96cb775459d9cefccbf888c38f30989cc25e6e96
원준 <@DESKTOP-A2F4S63>
2021-11-17 15:29:38 +0900

    첫 깃허브

--- Contract/src/ContractCheck.java
+++ Contract/src/ContractCheck.java
@@ -1,22 +1,33 @@
 import java.io.File;
+import java.util.ArrayList;
 
 public class ContractCheck {
 
-	public static void main(String[] args) {
-		  folderInFiles("F:\\eclipse\\workspace");
+	static ArrayList<String> Flist2 = new ArrayList<>();
+	
+	public static void main(String[] args) { 
+		String DirPath = "Z:\\전원준\\2021정산\\2021년 강원본부 부정기 외부계약 및 정산\\외부 계약현황"; 
+		FileDetect(DirPath); 
+	}
+	
+	public static void FileDetect( String DirPath ){
+		File path = new File( DirPath ); 
+		File[] FList = path.listFiles(); 
+		
+		
+		for( int i = 0; i < FList.length; i++ ){
+			if( FList[i].isDirectory() )
+				{ 
+					Flist2.add(DirPath.concat(FList[i].getName()));
+					System.out.println( Flist2.get(i) ); // 파일의 FullPath 출력 
+				}
+			else break;
 		}
-
-		public static void folderInFiles(String path) {
-		  File folder = new File(path);
-		  File files[] = folder.listFiles();
-
-		  for(int i=0; i<files.length; i++) {
-		    File file = files[i];
-		    if(file.isDirectory()) {
-		      folderInFiles(file.getPath());
-		    } else {
-		      System.out.println(files[i]);
-		    }
-		  }
-		}
+	}
 }
+
+
+//https://sgpassion.tistory.com/331
+		
+		
+
